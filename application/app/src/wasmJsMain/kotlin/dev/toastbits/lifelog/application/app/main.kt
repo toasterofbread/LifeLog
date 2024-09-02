9ef710f7adbd313a93a70589dc1d67f9951fdfc5
toasterofbread <talohalton@gmail.com>
2024-09-02 17:32:34 +0100

    Store prefs as cookies in WasmJS target

--- application/app/src/wasmJsMain/kotlin/dev/toastbits/lifelog/application/app/main.kt
+++ application/app/src/wasmJsMain/kotlin/dev/toastbits/lifelog/application/app/main.kt
@@ -1,9 +1,11 @@
 package dev.toastbits.lifelog.application.app
 
 import androidx.compose.ui.window.CanvasBasedWindow
-import dev.toastbits.composekit.platform.InMemoryPlatformPreferences
+import com.toasterofbread.composekit.platform.BrowserCookies
+import com.toasterofbread.composekit.platform.CookiesPlatformPreferences
 import dev.toastbits.composekit.platform.PlatformContext
 import dev.toastbits.composekit.platform.PlatformContextImpl
+import dev.toastbits.composekit.platform.PlatformPreferences
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 
@@ -11,7 +13,7 @@ fun main() {
     val coroutineScope: CoroutineScope = CoroutineScope(Job())
 
     val context: PlatformContext = PlatformContextImpl(coroutineScope)
-    val prefs: InMemoryPlatformPreferences = InMemoryPlatformPreferences()
+    val prefs: PlatformPreferences = CookiesPlatformPreferences(BrowserCookies)
 
     val application: Application = Application(context, prefs)
 
